718d8daf3cefc81bf723c71cda9c6a9c9007117d
sogrey <suzhaoheng2006@163.com>
2022-05-30 01:03:50 +0800

    init three.js

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+// import HelloWorld from './components/HelloWorld.vue'
+import ThreeView from './components/ThreeView.vue'
+</script>
+
+<template>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+
+    <ThreeView />
+</template>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+}
+</style>
