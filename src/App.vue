a3dbe0ae658873f66b57145634f35173a02d28c5
Sogrey <suzhaoheng2006@163.com>
2022-05-30 10:21:50 +0800

    打包优化

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,14 @@ import ThreeView from './components/ThreeView.vue'
 </script>
 
 <template>
-    <ThreeView  />
+  <ThreeView />
 </template>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
 }
 </style>
