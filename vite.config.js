0bc4076cbdacbf78a1c7c639ebffd8b89370282d
Sogrey <suzhaoheng2006@163.com>
2022-05-30 15:07:30 +0800

    update vite configure

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const path = require('path')
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [vue()],
+  alias: {
+    // 键必须以斜线开始和结束
+    '/@/': path.resolve(__dirname, './src'),
+  },
+  // 打包配置
   build: {
+    target: 'modules',
+    outDir: 'dist', //指定输出路径
     sourcemap: false,
-    minify: 'terser',
+    assetsDir: 'assets', // 指定生成静态资源的存放路径
+    minify: 'terser',// 混淆器，terser构建后文件体积更小
     chunkSizeWarningLimit: 1500,
     terserOptions: {
       compress: {
